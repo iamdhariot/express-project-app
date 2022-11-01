6d6442bf7ff37480ed7d2c7aced25a65fcb6eb73
iamdhariot <iamdhariot@gmail.com>
2022-11-01 11:24:31 +0530

    express app folder structure type wise

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ require("rootpath")();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
+if (process.env.PORT !== "production") {
+  require("dotenv").config();
+}
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App is running on http://localhost:${port}`);
+});
